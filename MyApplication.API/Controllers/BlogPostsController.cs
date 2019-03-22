c6c54d7ed4fb168050c91c00ce8dc86f0f92a57c
Thijs Dickmans <thijsdickmans@gmail.com>
2019-03-22 23:56:03 +0100

    Documented the API controller

--- MyApplication.API/Controllers/BlogPostsController.cs
+++ MyApplication.API/Controllers/BlogPostsController.cs
@@ -11,6 +11,7 @@ namespace MyApplication.API.Controllers
     /// </summary>
     [ApiController]
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class BlogPostsController : ControllerBase
     {
         #region SERVICES
@@ -23,14 +24,26 @@ namespace MyApplication.API.Controllers
         #endregion
 
         #region GET     /
+        /// <summary>
+        /// Get a list of all blog posts.
+        /// </summary>
+        /// <returns>A list of all blog posts.</returns>
+        /// <response code="200">Returns the list of all blog posts</response>
         [HttpGet]
-        public ActionResult<IEnumerable<BlogPost>> Get()
+        public ActionResult<IEnumerable<BlogPost>> GetAll()
         {
             return _blogPosts;
         }
         #endregion
         
         #region GET     /{id}
+        /// <summary>
+        /// Get a single blog post.
+        /// </summary>
+        /// <param name="id">Id of the blog post</param>
+        /// <returns>A single blog post.</returns>
+        /// <response code="200">Returns a single blog post</response>
+        /// <response code="404">No blog post found for the given id</response>
         [HttpGet("{id}")]
         public ActionResult<BlogPost> Get(int id)
         {
@@ -43,8 +56,15 @@ namespace MyApplication.API.Controllers
         #endregion
 
         #region POST    /
+        /// <summary>
+        /// Create a new blog post.
+        /// </summary>
+        /// <param name="createBlogPost">Data for the new blog post</param>
+        /// <returns>The newly created blog post</returns>
+        /// <response code="200">Blog post succesfully created</response>
+        /// <response code="400">Validation error</response>
         [HttpPost]
-        public void Create([FromBody] CreateBlogPost createBlogPost)
+        public ActionResult<BlogPost> Create([FromBody] CreateBlogPost createBlogPost)
         {
             var newBlogPost = new BlogPost()
             {
@@ -53,10 +73,20 @@ namespace MyApplication.API.Controllers
                 Text = createBlogPost.Text,
             };
             _blogPosts.Add(newBlogPost);
+            return newBlogPost;
         }
         #endregion
 
         #region PUT     /{id}
+        /// <summary>
+        /// Update an existing blog post.
+        /// </summary>
+        /// <param name="id">Id of the blog post to update</param>
+        /// <param name="updatedBlogPost">New values for the blog post</param>
+        /// <returns>Returns the updated blog post.</returns>
+        /// <response code="200">Blog post succesfully updated</response>
+        /// <response code="400">Validation error</response>
+        /// <response code="404">No blog posts found with the given id</response>
         [HttpPut("{id}")]
         public ActionResult<BlogPost> Update(
             int id,
@@ -82,6 +112,12 @@ namespace MyApplication.API.Controllers
         #endregion
 
         #region DELETE  /{id}
+        /// <summary>
+        /// Delete an existing blog post.
+        /// </summary>
+        /// <param name="id">Id of the blog post to delete</param>
+        /// <response code="200">Blog post succesfully deleted</response>
+        /// <response code="404">No blog posts found with the given id</response>
         [HttpDelete("{id}")]
         public ActionResult Delete(int id)
         {
@@ -98,7 +134,7 @@ namespace MyApplication.API.Controllers
                 {
                     return NotFound();
                 }
-                return BadRequest();
+                throw;
             }
         }
         #endregion
